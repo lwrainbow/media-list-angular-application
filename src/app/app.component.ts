5375dd50858ac8064fc6a2ba8b024c14bdb31334
Xiaoyu Liang <emerald@Xiaoyus-MacBook-Pro.local>
2019-12-29 21:20:11 -0500

    change inline template to templateUrl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,12 @@ import { Component } from '@angular/core'; //Component decorator
 
 @Component({
   selector: 'mw-app', // Use to locate a custom html element and apply the component to (index.html)
-  template: '<h1>My App</h1>'
+  // Inline template
+  // template: `
+  //   <h1>My App</h1>
+  //   <p>Keeping track of the media I want to watch.</p>
+  // `
+
+  templateUrl: './app.component.html'
 })
 export class AppComponent { }
