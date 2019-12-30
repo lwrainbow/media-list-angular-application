e8483c8a91f6e3faf35c3c356bea493f9bdc99cb
Xiaoyu Liang <emerald@Xiaoyus-MacBook-Pro.local>
2019-12-29 21:10:56 -0500

    change name of component selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core'; //Component decorator
 
 @Component({
-  selector: 'app-root', // Use to locate a custom html element and apply the component to (index.html)
+  selector: 'mw-app', // Use to locate a custom html element and apply the component to (index.html)
   template: '<h1>My App</h1>'
 })
 export class AppComponent { }
