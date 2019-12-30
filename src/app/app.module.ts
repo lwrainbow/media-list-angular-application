1b3daac71f5149509f74c1dbc5d7afd7f9299a16
Xiaoyu Liang <emerald@Xiaoyus-MacBook-Pro.local>
2019-12-29 20:22:51 -0500

    NgModule and the root module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core'; //NgModule decorator
+import { BrowserModule } from '@angular/platform-browser'; //Contains core directives, pipes, and more for working with the DOM
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
+  // Make components, directives, and pipes available to module that don't come from another module
   declarations: [
     AppComponent
-  ],
+  ], 
+  // Bring in other Angular modules
   imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    BrowserModule
+  ], 
   providers: [],
-  bootstrap: [AppComponent]
+  //Forroot module + will let Angular know which components will be the starting point for the bootstrap process
+  bootstrap: [
+    AppComponent
+  ] //app.component.ts
 })
 export class AppModule { }
