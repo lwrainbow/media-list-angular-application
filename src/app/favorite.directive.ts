911d15a8fcd82f2f2b04883ebdcc045c8c1b3fcc
Emerald <emerald.xiaoyuliang@gmail.com>
2020-01-05 02:03:03 -0500

    add hovering event to mwFavorite custom attribute directive

--- src/app/favorite.directive.ts
+++ src/app/favorite.directive.ts
@@ -1,5 +1,5 @@
 // HostBinding bind a host element property to a directive property
-import { Directive, HostBinding, Input } from '@angular/core';
+import { Directive, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[mwFavorite]'
@@ -9,5 +9,13 @@ export class FavoriteDirective {
   @HostBinding('class.is-favorite') isFavorite = true; // add class is-favorite when isFavorite equal true
   @Input() set mwFavorite(value) {
     this.isFavorite = value;
+  }
+
+  @HostBinding('class.is-favorite-hovering') hovering = false;
+  @HostListener('mouseenter') onMouseenter() {
+    this.hovering = true;
+  }
+  @HostListener('mouseleave') onMouseLeave() {
+    this.hovering = false;
   }
 }
