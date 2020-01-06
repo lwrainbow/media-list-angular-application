f7c467c130b766acc7d2e425e7fa941f20adbd00
Emerald <emerald.xiaoyuliang@gmail.com>
2020-01-05 22:16:32 -0500

    add template-driven form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core'; //NgModule decorator
 import { BrowserModule } from '@angular/platform-browser'; //Contains core directives, pipes, and more for working with the DOM
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
 import { MediaItemListComponent } from './media-item-list.component';
+import { MediaItemFormComponent } from './media-item-form.component';
 
 import { FavoriteDirective } from './favorite.directive';
 
@@ -15,12 +17,14 @@ import { CategoryListPipe } from './category-list.pipe';
     AppComponent, 
     MediaItemComponent,
     MediaItemListComponent, 
+    MediaItemFormComponent, 
     FavoriteDirective,
     CategoryListPipe
   ], 
   // Bring in other Angular modules
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ], 
   providers: [],
   //Forroot module + will let Angular know which components will be the starting point for the bootstrap process
